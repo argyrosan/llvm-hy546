42cadcfaa097a2eceb507ef7fe4d7f06c04e9938
Anargyros Argyros <argyrosan@gmail.com>
2025-01-29 12:06:13 +0200

    final version

--- llvm/lib/Transforms/Utils/PrintfPass.cpp
+++ llvm/lib/Transforms/Utils/PrintfPass.cpp
@@ -2,12 +2,17 @@
 
 using namespace llvm;
 
+// module pass because we need to declare functions/global variables
 PreservedAnalyses PrintfPass::run(Module &M, ModuleAnalysisManager &AM) {
+  // construct a generic integer pointer type
   PointerType *PointerTy =
       PointerType::get(IntegerType::get(M.getContext(), 32), 0);
+
   // declare fopen as an externally linked function
+  // construct type
   auto *fopen_type =
       FunctionType::get(PointerTy, {PointerTy, PointerTy}, false);
+  // insert function into module
   auto fopen_value = M.getOrInsertFunction("fopen", fopen_type).getCallee();
   auto fopen_funct = dyn_cast<Function>(fopen_value);
 
@@ -18,12 +23,13 @@ PreservedAnalyses PrintfPass::run(Module &M, ModuleAnalysisManager &AM) {
       M.getOrInsertFunction("fprintf", fprintf_type).getCallee();
   auto fprintf_funct = dyn_cast<Function>(fprintf_value);
 
-  // declare fclose
+  // declare fclose as an externally linked function
   auto fclose_type = FunctionType::get(IntegerType::get(M.getContext(), 32),
                                        {PointerTy}, false);
   auto fclose_value = M.getOrInsertFunction("fclose", fclose_type).getCallee();
   auto fclose_funct = dyn_cast<Function>(fclose_value);
 
+  // declare a global variable to keep file descriptor for log.txt
   GlobalVariable *global_file_descriptor =
       new GlobalVariable(/*Module=*/M,
                          /*Type=*/PointerTy,
@@ -31,9 +37,10 @@ PreservedAnalyses PrintfPass::run(Module &M, ModuleAnalysisManager &AM) {
                          /*Linkage=*/GlobalValue::CommonLinkage,
                          /*Initializer=*/0, // has initializer, specified below
                          /*Name=*/"log_file_descriptor");
-
   ConstantPointerNull *const_ptr_2 = ConstantPointerNull::get(PointerTy);
   global_file_descriptor->setInitializer(const_ptr_2);
+
+  // iterate through all functions in module
   for (auto &F : M) {
     // encounter main function
     if (F.getName() == "main") {
@@ -43,10 +50,10 @@ PreservedAnalyses PrintfPass::run(Module &M, ModuleAnalysisManager &AM) {
       auto first_instr = BB->getFirstNonPHI();
       builder.SetInsertPoint(first_instr);
       llvm::Value *filename = builder.CreateGlobalString("log.txt");
-      llvm::Value *openmode = builder.CreateGlobalString("r");
+      llvm::Value *openmode = builder.CreateGlobalString("w");
       auto fopen_call_instr =
           builder.CreateCall(fopen_funct, {filename, openmode});
-      builder.CreateStore(global_file_descriptor, fopen_call_instr);
+      builder.CreateStore(fopen_call_instr, global_file_descriptor);
 
       // iterate through basic blocks -> call fclose before each return
       // statement
@@ -71,10 +78,10 @@ PreservedAnalyses PrintfPass::run(Module &M, ModuleAnalysisManager &AM) {
             IRBuilder<> builder(CB);
             builder.SetInsertPoint(CB);
             std::vector<llvm::Value *> arg_vec;
-            auto file_desc = builder.CreateLoad(PointerTy,global_file_descriptor);
+            auto file_desc =
+                builder.CreateLoad(PointerTy, global_file_descriptor);
             arg_vec.push_back(file_desc);
-            for (auto arg = CB->arg_begin();
-                 arg != CB->arg_end(); ++arg) {
+            for (auto arg = CB->arg_begin(); arg != CB->arg_end(); ++arg) {
               auto val = dyn_cast<llvm::Value>(arg);
               arg_vec.push_back(val);
             }
